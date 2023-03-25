a51a2dec3b316809400d0d153e61855f145eb411
igor <igor.240340@gmail.com>
2023-03-25 22:20:41 +0700

    Refactor

--- CLOPEClusteringAlgorithm/CLOPEClusteringAlgorithm.cpp
+++ CLOPEClusteringAlgorithm/CLOPEClusteringAlgorithm.cpp
@@ -6,53 +6,44 @@
 // Применяет кластеризацию к датасету dataset с параметром repulsion.
 //
 unsigned short CLOPEClusteringAlgorithm::Apply(Dataset& dataset, const float repulsion) {
-    unsigned short iterationCount = 0;
     std::unordered_map<int, Cluster*> idToCluster;
     Cluster emptyCluster;
-    std::list<Cluster> clusters = { emptyCluster };
+    std::list<Cluster> clusters = { emptyCluster }; // Пустой кластер - для оценки прироста целевой функции при добавлении транзакции в новый кластер.
 
-    // Первичное распределение по кластерам - фаза инициализации.
-    Transaction transac;
-    while (dataset.ReadNextTransaction(transac)) {
-        Cluster* maxCluster = FindMaxCluster(clusters, transac, repulsion);
-
-        // Если целевая функция максимизируется добавлением в пустой кластер,
-        // то создаем новый кластер, а имеющийся пустой оставляем нетронутым для дальнейших проверок.
-        if ((*maxCluster).IsEmpty()) {
-            Cluster newCluster;
-            newCluster.Add(transac, repulsion);
-            clusters.push_back(newCluster);
-            idToCluster[newCluster.id] = &clusters.back();
-        }
-        else {
-            (*maxCluster).Add(transac, repulsion);
-        }
-
-        dataset.WriteTransaction(transac);
-    }
-    iterationCount++;
-
-    // Дальнейшая максимизация целевой функции.
-    bool thereWasMove;  // Признак того, что хотя бы одна транзакция была перемещена между кластерами во время итерации.
+    unsigned short iterationCount = 0;
+    bool thereWasMove; // Признак того, что хотя бы одна транзакция была перемещена между кластерами во время итерации.
     do {
         thereWasMove = false;
-        dataset.Reopen(); // Переоткрываем выходной файл с предыдущей итерации как входной.
+        bool initPhase = iterationCount == 0;   // На фазе инициализации добавление в любой кластер считается перемещением транзакции и thereWasMove устанавливается в true.
 
+        // Переоткрываем выходной файл с предыдущей итерации как входной.
+        if (!initPhase) {
+            dataset.Reopen();
+        }
+
+        Transaction transac;
         while (dataset.ReadNextTransaction(transac)) {
             // Удаляем транзакцию из её текущего кластера.
             // Если кластер стал пустым, то удаляем и сам кластер.
-            Cluster& prevCluster = *(idToCluster[transac.clusterId]);
-            int prevClusterId = prevCluster.id;
-            prevCluster.Remove(transac, repulsion);
-            bool prevClusterWasEmpty = false;
-            if (prevCluster.IsEmpty()) {
-                prevClusterWasEmpty = true;
-                idToCluster.erase(prevClusterId);
-                clusters.remove(prevCluster);
+            int prevClusterId;
+            bool prevClusterWasEmpty;
+            if (!initPhase) {
+                Cluster& prevCluster = *(idToCluster[transac.clusterId]);
+                prevClusterId = prevCluster.id;
+                prevCluster.Remove(transac, repulsion);
+                prevClusterWasEmpty = false;
+
+                if (prevCluster.IsEmpty()) {
+                    prevClusterWasEmpty = true;
+                    idToCluster.erase(prevClusterId);
+                    clusters.remove(prevCluster);
+                }
             }
 
             Cluster* maxCluster = FindMaxCluster(clusters, transac, repulsion);
 
+            // Если целевая функция максимизируется добавлением в пустой кластер,
+            // то создаем новый кластер, а имеющийся пустой оставляем нетронутым для дальнейших проверок.
             if ((*maxCluster).IsEmpty()) {
                 Cluster newCluster;
                 newCluster.Add(transac, repulsion);
@@ -62,9 +53,9 @@ unsigned short CLOPEClusteringAlgorithm::Apply(Dataset& dataset, const float rep
                 // Если текущая транзакция была единственной в предыдущем кластере,
                 // то добавление её в новый пустой кластер эквивалентно возвращению в прежний,
                 // что в свою очередь эквивалентно отсутствию перемещения этой транзакции.
-                if (!prevClusterWasEmpty) {
+                if (initPhase || !prevClusterWasEmpty) {
                     // NOTE:
-                    // Мы не попадаем в эту ветку на текущем тестовом датасете.
+                    // Мы не попадаем в эту ветку на текущем тестовом датасете на фазе максимизации.
                     // То есть не возникает ситуация, когда удаление транзакции из существующего кластера (который содержит транзакций больше чем 1)
                     // и помещение её в новый пустой максимизирует целевую функцию.
                     thereWasMove = true;
@@ -74,13 +65,14 @@ unsigned short CLOPEClusteringAlgorithm::Apply(Dataset& dataset, const float rep
                 (*maxCluster).Add(transac, repulsion);
 
                 // Если произошел возврат в прежний кластер, то перемещения не было.
-                if ((*maxCluster).id != prevClusterId) {
+                if (initPhase || (*maxCluster).id != prevClusterId) {
                     thereWasMove = true;
                 }
             }
 
             dataset.WriteTransaction(transac);
         }
+
         iterationCount++;
     } while (thereWasMove);
 
