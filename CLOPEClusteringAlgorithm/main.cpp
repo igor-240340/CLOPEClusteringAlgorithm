e468911cedbc956b666ff924f54930345dc1b690
igor <igor.240340@gmail.com>
2023-04-13 00:27:13 +0700

    Refactor

--- CLOPEClusteringAlgorithm/main.cpp
+++ CLOPEClusteringAlgorithm/main.cpp
@@ -9,13 +9,13 @@ static const float MUSHROOM_REPULSION = 2.6f;
 static const float BOOK_REPULSION = 1.3f;
 static const float WORD_REPULSION = 2.2f;
 
-int main()
+void main()
 {
     MushroomDataset mushrooms("./Data/agaricus-lepiota.data");
     unsigned short iterationCount = CLOPEClusteringAlgorithm::Perform(mushrooms, MUSHROOM_REPULSION);
     std::cout << "Iterations for mushrooms: " << iterationCount << std::endl;
 
     BookDataset books("../CLOPEClusteringAlgorithm/Data/books.txt", WORD_REPULSION);
-    iterationCount = CLOPEClusteringAlgorithm::Perform(books, 1.3f);
+    iterationCount = CLOPEClusteringAlgorithm::Perform(books, BOOK_REPULSION);
     std::cout << "Iterations for books: " << iterationCount << std::endl;
 }
